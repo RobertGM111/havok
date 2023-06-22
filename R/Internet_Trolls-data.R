f626c0080a96dc018e3d3c918cb2bbb1a7b1325b
Robert Moulder <robertmoulder@MacBook-Pro-104.local>
2023-06-22 08:38:49 -0600

    Additional data of Russian troll tweets

---
+++ R/Internet_Trolls-data.R
@@ -0,0 +1,42 @@
+#' Topic scores of right-wing Russian troll Twitter activity
+#'
+#'
+#' 1453 scores on 11 Topics.
+#'
+#' @name Internet_Trolls
+#'
+#' @docType data
+#'
+#' @usage data(Internet_Trolls)
+#'
+#' @format A data frame with 1453 rows and 16 variables:
+#' \describe{
+#'   \item{Date}{TO-DO}
+#'   \item{Time_of_day}{TO-DO}
+#'   \item{Type}{TO-DO}
+#'   \item{Topic1}{TO-DO}
+#'   \item{Topic2}{TO-DO}
+#'   \item{Topic3}{TO-DO}
+#'   \item{Topic4}{TO-DO}
+#'   \item{Topic5}{TO-DO}
+#'   \item{Topic6}{TO-DO}
+#'   \item{Topic7}{TO-DO}
+#'   \item{Topic8}{TO-DO}
+#'   \item{Topic9}{TO-DO}
+#'   \item{Topic10}{TO-DO}
+#'   \item{Topic11}{TO-DO}
+#'   \item{author}{TO-DO}
+#'   \item{Time_stamp}{TO-DO}
+#'   }
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://github.com/fivethirtyeight/russian-troll-tweets/}{www.fivethirtyeight.com's github}
+#'
+#' @examples
+#' \donttest{
+#' data(Internet_Trolls)
+#' }
+NULL
+
+
